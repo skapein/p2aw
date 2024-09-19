519fb5cf99f56990acc40943e7ebfa068da0c32a
chrisMJ <chris666.10@hotmail.com>
2024-09-19 01:48:55 -0500

    fix

---
+++ Plantilla/src/app/interfaces/ievento.ts
@@ -0,0 +1,8 @@
+export interface IEvento {
+    evento_id: number;
+    nombre: string;
+    descripcion: string;
+    fecha: string; // O Date si lo prefieres como objeto de fecha
+    ubicacion: string;
+  }
+  
