f6e4f34af4b760057597f5a9b4b519e8ceebf57e
chrisMJ <chris666.10@hotmail.com>
2024-09-18 22:57:02 -0500

    correccion

--- Plantilla/src/app/reservaciones/reservaciones.component.ts
+++ Plantilla/src/app/reservaciones/reservaciones.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservaService } from '../services/reserva.service'; // Asegúrate de que la ruta sea correcta
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservaciones',
-  standalone: true,
-  imports: [],
   templateUrl: './reservaciones.component.html',
-  styleUrl: './reservaciones.component.css'
+  styleUrls: ['./reservaciones.component.scss']
 })
-export class ReservacionesComponent {
+export class ReservacionesComponent implements OnInit {
+  reservas: any[] = [];
 
+  constructor(
+    private reservaService: ReservaService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.obtenerReservas();
+  }
+
+  obtenerReservas(): void {
+    this.reservaService.obtenerReservas().subscribe(
+      response => {
+        this.reservas = response;
+      },
+      error => {
+        console.error('Error al obtener las reservas', error);
+      }
+    );
+  }
+
+  crearReserva(): void {
+    this.router.navigate(['/nueva-reserva']); // Ajusta la ruta según tu configuración
+  }
+
+  verDetalles(reserva: any): void {
+    this.router.navigate(['/reserva-detalle', reserva.reserva_id]); // Ajusta la ruta según tu configuración
+  }
+
+  eliminarReserva(reservaId: number): void {
+    if (confirm('¿Estás seguro de que deseas eliminar esta reserva?')) {
+      this.reservaService.eliminarReserva(reservaId).subscribe(
+        () => {
+          this.obtenerReservas(); // Actualiza la lista después de eliminar
+          alert('Reserva eliminada con éxito');
+        },
+        error => {
+          console.error('Error al eliminar la reserva', error);
+          alert('Hubo un error al eliminar la reserva');
+        }
+      );
+    }
+  }
 }
